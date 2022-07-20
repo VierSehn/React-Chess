80e21c0ac937084df318a475e2bc990f5f624cc2
VierSehn <beberovitchrg@gmail.com>
2022-07-20 21:22:19 +0300

    inialCreatePieces function

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,12 +2,7 @@ import React from "react";
 
 import Square from "./Square";
 import "./Board.css";
-import Pawn from "../../pieces/Pawn";
-import Rook from "../../pieces/Rook";
-import King from "../../pieces/King";
-import Knight from "../../pieces/Knight";
-import Bishop from "../../pieces/Bishop";
-import Queen from "../../pieces/Queen";
+import createInitialPieces from "../../createInitialPieces";
 
 const getOppositeTeam = (currentTeam) => {
   if (currentTeam === "white") {
@@ -21,40 +16,7 @@ const getOppositeTeam = (currentTeam) => {
 
 class Board extends React.Component {
   state = {
-    pieces: [
-      new Rook(0, 0, "black"),
-      new Knight(1, 0, "black"),
-      new Bishop(2, 0, "black"),
-      new Queen(3, 0, "black"),
-      new King(4, 0, "black"),
-      new Bishop(5, 0, "black"),
-      new Knight(6, 0, "black"),
-      new Rook(7, 0, "black"),
-      new Pawn(0, 1, "black"),
-      new Pawn(1, 1, "black"),
-      new Pawn(2, 1, "black"),
-      new Pawn(3, 1, "black"),
-      new Pawn(4, 1, "black"),
-      new Pawn(5, 1, "black"),
-      new Pawn(6, 1, "black"),
-      new Pawn(7, 1, "black"),
-      new Rook(0, 7, "white"),
-      new Knight(1, 7, "white"),
-      new Bishop(2, 7, "white"),
-      new Queen(3, 7, "white"),
-      new King(4, 7, "white"),
-      new Bishop(5, 7, "white"),
-      new Knight(6, 7, "white"),
-      new Rook(7, 7, "white"),
-      new Pawn(0, 6, "white"),
-      new Pawn(1, 6, "white"),
-      new Pawn(2, 6, "white"),
-      new Pawn(3, 6, "white"),
-      new Pawn(4, 6, "white"),
-      new Pawn(5, 6, "white"),
-      new Pawn(6, 6, "white"),
-      new Pawn(7, 6, "white"),
-    ],
+    pieces: createInitialPieces(),
     selectedPiece: null,
     currentTeam: "white",
   };
